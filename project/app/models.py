c4bbb84aaf9cdf4aea24a7362968ec7e113981d3
ryan-lam <ryan-lam@users.noreply.github.com>
2021-05-22 21:02:12 -0400

    more db and views.py

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
 
+# Item listing database
 class Item(models.Model):
     condition_choices = [
         ("Brand New", "Brand New"),
@@ -11,6 +11,7 @@ class Item(models.Model):
         ]
 
     poster = models.CharField(max_length=265, null=False, blank=False)
+    code = models.CharField(max_length=12, null=False, blank=False)
     # status: true means available, false means not available
     status = models.BooleanField(default=True)
     title = models.CharField(max_length=265, null=False, blank=False)
@@ -18,4 +19,25 @@ class Item(models.Model):
     location = models.CharField(max_length=128, null=False, blank=False)
     description = models.CharField(max_length=2048, null=False, blank=False)
 
-class User(models.Model)
+    def __str__(self):
+        return f"{self.poster} : {self.status} : {self.title}"
+
+
+# User profiles
+class User(models.Model):
+    username = CharField(max_length=32, null=False, blank=False)
+    password = CharField(max_length=32, null=False, blank=False)
+    name = CharField(max_length=32, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.name} : {self.username}"
+
+
+class Image(models.Model):
+    code = models.CharField(max_length=12, null=False, blank=False)
+    image = models.FileField(upload_to="images/")
+    # <input type="file" name="images" multiple>
+
+    def __str__(self):
+        return f"{self.code}"
+
--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django import session
+import random
+
+def generate_code():
+	''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(12))
+
 
 # Create your views here.
 
 def index(request):
-	return render(request, "index.html")
-	
+	return render(request, "index.HTML")
+
+def marketplace(request):
+	items = Item.objects.all.filter(status=True)
+	return render(request, "marketplace.HTML", {
+		"items":items
+		})
+
+
+
+
+# Done
+def add_item(request):
+	if "username" in request.session:
+		return render(request, "add_item.HTML")
+	else:
+		return HttpResponseRedirect(reverse(index))
+
+
+# Done
+def add_item_submit(request):
+	if request.method == "POST":
+		poster = request.session["username"]
+		title = request.POST["title"]
+		condition = request.POST["title"]
+		location = request.POST["location"]
+		description = request.POST["description"]
+
+		new_item = Item(poster=poster, title=title, condition=condition, location=location, description=description)
+		new_item.save()
+
+		return HttpResponseRedirect(reverse(marketplace), {
+			"add_item": True
+			})
+
+	else:
+		return HttpResponseRedirect(reverse(add_item))
+
+
+
+def logout(request):
+	request.session.flush()
+	return HttpResponseRedirect(reverse(index))
+
